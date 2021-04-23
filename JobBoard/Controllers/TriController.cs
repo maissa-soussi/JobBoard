ecccf9a669520d4d500697dd21d02a2e97425415
ahmedbenalaya30 <ahmed.benalaya@ensi-uma.tn>
2021-04-23 17:49:23 +0000

    Add TriController

---
+++ JobBoard/Controllers/TriController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Text;
+using Newtonsoft.Json;
+using JobBoard.Models;
+
+
+namespace JobBoard.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class TriController : ControllerBase
+    {
+        HttpClientHandler _clientHandler = new HttpClientHandler();
+
+        DiplomaDTO _oDiploma = new DiplomaDTO();
+        List<int >_idCandidats =new List<int>();
+
+        public TriController()
+        {
+            _clientHandler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; };
+        }
+
+        [HttpGet("{id}")]
+        public async Task<List<int>> GetDiplomaDTO(int id)
+        {
+            using (var httpClient = new HttpClient(_clientHandler))
+            {
+                using (var response = await httpClient.GetAsync("http://localhost:5000/predict/" + id))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    _idCandidats = JsonConvert.DeserializeObject<List<int>>(apiResponse);
+                }
+            }
+            return _idCandidats;
+        }
+    }
+}
