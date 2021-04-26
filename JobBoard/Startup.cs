42c6e058328892a23f4454075fbfb124eebdd0df
farouk kassebi <farouk.kassebi@ensi-uma.tn>
2021-04-26 16:08:06 +0100

    addcors

--- JobBoard/Startup.cs
+++ JobBoard/Startup.cs
@@ -26,11 +26,16 @@ namespace JobBoard
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors(options =>
+                options.WithOrigins("http://localhost:4200")
+                .AllowAnyMethod()
+                .AllowAnyHeader());
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
