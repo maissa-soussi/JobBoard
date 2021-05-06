eb84ee6d2c6b8aa8f1dc05988896e94c1f9c9694
ahmedbenalaya30 <ahmed.benalaya@ensi-uma.tn>
2021-05-06 00:15:24 +0000

    MyCandidaturesController & GetCandidaturesController

---
+++ JobBoard/Controllers/GetCandidateController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Text;
+using Newtonsoft.Json;
+using JobBoard.Models;
+
+
+namespace JobBoard.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class GetCandidateController : ControllerBase
+    {
+        HttpClientHandler _clientHandler = new HttpClientHandler();
+        Candidate _oCandidate = new Candidate();
+
+        public GetCandidateController()
+        {
+            _clientHandler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; };
+        }
+
+        [HttpGet("{id}")]
+        public async Task<Candidate> GetCandidate(int id)
+        {
+            _oCandidate = new Candidate();
+            using (var httpClient = new HttpClient(_clientHandler))
+            {
+                using (var response = await httpClient.GetAsync("https://localhost:44304/api/GetCandidate/" + id))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    _oCandidate = JsonConvert.DeserializeObject<Candidate>(apiResponse);
+                }
+            }
+            return _oCandidate;
+        }
+    }
+}
---
+++ JobBoard/Controllers/MyCandidaturesController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Text;
+using Newtonsoft.Json;
+using JobBoard.Models;
+
+namespace JobBoard.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class MyCandidaturesController : ControllerBase
+    {
+        HttpClientHandler _clientHandler = new HttpClientHandler();
+        List<Candidature> _oCandidatures = new List<Candidature>();
+
+        public MyCandidaturesController()
+        {
+            _clientHandler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; };
+        }
+
+        [HttpGet("{id}")]
+        public async Task<List<Candidature>> GetCandidature(int id)
+        {
+            _oCandidatures = new List<Candidature>();
+            using (var httpClient = new HttpClient(_clientHandler))
+            {
+                using (var response = await httpClient.GetAsync("https://localhost:44304/api/MyCandidatures/"+id))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    _oCandidatures = JsonConvert.DeserializeObject<List<Candidature>>(apiResponse);
+                }
+            }
+            return _oCandidatures;
+        }
+    }
+}
